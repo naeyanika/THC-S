6aa83d2f278630144857b65c604c318e979f6cb0
naeyanika <40598758+naeyanika@users.noreply.github.com>
2024-08-01 10:58:50 +0700

    Update THC_S.py

--- THC_S.py
+++ THC_S.py
@@ -81,10 +81,8 @@ if 'DbSimpanan.xlsx' in dfs and 'THC.xlsx' in dfs:
     df1 = pd.read_excel('THC S.xlsx')
 
     selected_columns = ['ID', 'NAMA', 'CENTER', 'KEL', 'Db Sihara', 'Cr Sihara']
-    df1_selected_1 = df_thc[selected_columns]
-        
-    st.write("Cek Data")
-    st.write(df1_selected_1)
+    df1_selected_1 = df1[selected_columns]
+
     df1_selected_1['Modus_Sihara'] = df1_selected_1.groupby(['ID', 'NAMA'])['Db Sihara'].transform(lambda x: x.mode()[0])
     
     df1_selected = df1_selected_1.loc[:, ['ID', 'NAMA', 'Modus_Sihara']]
