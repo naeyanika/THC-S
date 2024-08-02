47e2d711414ecf053feef4ff8d6640aa93ab0102
naeyanika <40598758+naeyanika@users.noreply.github.com>
2024-08-02 08:33:42 +0700

    Update THC_S.py

--- THC_S.py
+++ THC_S.py
@@ -146,7 +146,7 @@ if 'DbSimpanan.xlsx' in dfs and 'THC.xlsx' in dfs:
     merged_df2 = merged_df.merge(df1[['ID', 'Db Sihara', 'Cr Sihara']], left_on='ID Anggota', right_on='ID', how='left')
     merged_df2['Saldo Akhir'] = merged_df2['Saldo Sebelumnya'] + merged_df2['Db Sihara'] - merged_df2['Cr Sihara']
     merged_df2.drop(columns=['ID', 'Db Sihara', 'Cr Sihara'], inplace=True)
-    merged_df2.rename(columns=['Sisa': 'Selisih Transaksi'])
+    merged_df2.rename(columns={'Sisa': 'Selisih Transaksi'}, inplace=True)
 
     desired_order = [
         'ID Anggota','Nama','Center','Kelompok','Saldo Sebelumnya','Modus Sihara','Nilai Modus','Selisih Transaksi','Saldo Akhir','Total Transaksi','Transaksi Sesuai','Transaksi Nol','Transaksi Tidak Sesuai'
